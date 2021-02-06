c2a3b2afe65f43e204c4d85ddedb74395d211d37
PavelCherepanov <pavelk.cherepanov@gmail.com>
2021-02-06 23:21:34 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//кнопки
+$(".toggleButton").hover(function(){
+$(this).addClass("highlightedButton");
+}, function(){
+  $(this).removeClass("highlightedButton");
+});
+
+$(".toggleButton").click(function(){
+  $(this).toggleClass("active");
+  $(this).removeClass("highlightedButton");
+});
+//высота textarea
+$(".panel").height($(window).height() - $("#header").height() - 15);
+    
+$(".panel").width(($(window).width()/2) - 10);

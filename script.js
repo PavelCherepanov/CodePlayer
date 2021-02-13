796ef6e164b278e84909c0329f9d1c2492066f4f
PavelCherepanov <pavelk.cherepanov@gmail.com>
2021-02-13 22:22:30 +0700

    second commit

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function updateOutput(){
+  // html -> output
+  $("iframe").contents().find("html").html("<html><head><style type='text/css'>" + $("#cssPanel").val() + "</style></head><body>" + $("#htmlPanel").val() + "</body></html>");
+  document.getElementById("outputPanel").contentWindow.eval($("#jsPanel").val());
+  
+}
+
 //кнопки
 $(".toggleButton").hover(function(){
 $(this).addClass("highlightedButton");
@@ -8,8 +15,22 @@ $(this).addClass("highlightedButton");
 $(".toggleButton").click(function(){
   $(this).toggleClass("active");
   $(this).removeClass("highlightedButton");
+
+  // появление и исчезновение textarea
+  var panelId = $(this).attr("id") + "Panel";
+  $("#" + panelId).toggleClass("hidden");
+
+  // настраиваем ширину (кол-во textarea)
+  var numberOfActivePanels = 4 - $(".hidden").length;
+  $(".panel").width(($(window).width() / numberOfActivePanels) - 10);
 });
 //высота textarea
 $(".panel").height($(window).height() - $("#header").height() - 15);
-    
-$(".panel").width(($(window).width()/2) - 10);
+// ширина    
+$(".panel").width(($(window).width() / 2) - 10);
+
+updateOutput();
+
+$("textarea").on('change keyup paste', function(){
+  updateOutput();
+});
